43fa7e4b9ae56cdb7ba0311c9bf529320b2c533c
Shirley Cohen <scohen@cs.utexas.edu>
2018-11-09 11:15:34 -0600

    Create initial split tables from raw tables

---
+++ ctas_stmts.sql
@@ -0,0 +1,25 @@
+-- CTAS statements to create the initial college split tables from the raw tables. 
+
+create table college_split1.Class as
+select distinct cno, cname, credits 
+from college_raw.Teacher
+where cno is not null; 
+
+create table college_split1.Student as
+select distinct sid, fname, lname, dob
+from college_raw.Student;
+
+create table college_split1.Takes as
+select distinct sid, cno, grade
+from college_raw.Student
+where sid is not null and cno is not null;
+
+create table college_split1.Teacher as
+select distinct tid, instructor, dept
+from college_raw.Teacher
+where tid is not null; 
+
+create table college_split1.Teaches as
+select distinct tid, cno
+from college_raw.Teacher
+where tid is not null and cno is not null;
