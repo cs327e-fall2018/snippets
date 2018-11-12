19d425fac3413bfda1510c93f76c063da31fec21
Shirley Cohen <scohen@cs.utexas.edu>
2018-11-12 09:34:57 -0600

    Add files via upload

--- normalize_takes_cno.py
+++ normalize_takes_cno.py
@@ -4,7 +4,7 @@ from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 
 class NormalizeCno(beam.DoFn):
-  def process(self, element, cno_pcoll):
+  def process(self, element, class_pcoll):
     takes_record = element
     takes_cno = takes_record.get('cno')
     cno_splits = takes_cno.split(' ')
@@ -13,8 +13,8 @@ class NormalizeCno(beam.DoFn):
     cno_match = None
     
     for cno_split in cno_splits:
-        for cno_record in cno_pcoll:
-            class_cno = cno_record.get('cno')
+        for class_record in class_pcoll:
+            class_cno = class_record.get('cno')
             if (cno_split == class_cno):
                 found_cno_match = True
                 cno_match = cno_split
@@ -40,22 +40,31 @@ with beam.Pipeline('DirectRunner', options=opts) as p:
     # write PCollection to a log file
     takes_pcoll | 'Write to File 1' >> WriteToText('takes_query_results.txt')
     
-    cno_pcoll = p | 'Read Class' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT cno FROM college_split1.Class'))
+    class_pcoll = p | 'Read Class' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT cno, cname, credits FROM college_split1.Class'))
 
     # write PCollection to a log file
-    cno_pcoll | 'Write to File 2' >> WriteToText('cno_query_results.txt')
+    class_pcoll | 'Write to File 2' >> WriteToText('class_query_results.txt')
 
     # Flatten the two PCollections
-    normalized_pcoll = takes_pcoll | 'Normalize cno' >> beam.ParDo(NormalizeCno(), beam.pvalue.AsList(cno_pcoll)) 
+    normalized_pcoll = takes_pcoll | 'Normalize cno' >> beam.ParDo(NormalizeCno(), beam.pvalue.AsList(class_pcoll)) 
     
     # write PCollection to a file
     normalized_pcoll | 'Write to File 3' >> WriteToText('output_normalize_pardo.txt')
     
-    qualified_table_name = 'cs327e-fa2018:college_split2.Takes'
-    table_schema = 'sid:STRING,cno:STRING,grade:STRING'
+    qualified_takes_table_name = 'cs327e-fa2018:college_split2.Takes'
+    takes_table_schema = 'sid:STRING,cno:STRING,grade:STRING'
     
-    normalized_pcoll | 'Write to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_table_name, 
-                                                      schema=table_schema,  
+    normalized_pcoll | 'Write Takes to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_takes_table_name, 
+                                                      schema=takes_table_schema,  
                                                       create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
                                                       write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
+    
+    qualified_class_table_name = 'cs327e-fa2018:college_split2.Class'
+    class_table_schema = 'cno:STRING,cname:STRING,credits:INTEGER'
+    
+    class_pcoll | 'Write Class to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_class_table_name, 
+                                                      schema=class_table_schema,  
+                                                      create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                                                      write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
+    
 logging.getLogger().setLevel(logging.ERROR)
